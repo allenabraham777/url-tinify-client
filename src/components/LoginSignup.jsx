0af6bb5bbfe720f0f53aec3ebe4ed18ef5837c2c
Allen K Abraham <allen@theabstraction.co>
2020-07-28 00:22:18 +0530

    Final UI Tweaks

--- src/components/LoginSignup.jsx
+++ src/components/LoginSignup.jsx
@@ -3,7 +3,7 @@ import { NavLink, useHistory } from 'react-router-dom'
 import {isAuthenticated} from '../utils/isAuthenticated'
 const LoginSignup = (props) => {
   const [isLoggedIn, setIsLoggedIn] = useState(isAuthenticated())
-  const [message, setMessage] = useState("")
+  const [message, setMessage] = useState({success: "", error: ""})
   const [credentials, setCredentials] = useState({
     name: "",
     email: "",
@@ -32,19 +32,21 @@ const LoginSignup = (props) => {
     e.preventDefault()
     const response = await props.action(credentials)
     if(response.error) {
-      setMessage(response.error)
+      setMessage({success: "", error: response.error})
       return
     }
     if(tgt==="Login")
       setIsLoggedIn(true)
-    if(tgt==="SignUp")
+    if(tgt==="SignUp"){
       history.push('/user/login')
+      setMessage({success: "User Successfully Created", error: ""})
+    }
   }
 
   return (
     <form>
       {props.signup?<h1>SignUp</h1>:<h1>Login to Protal</h1>}
-      <h3>{message}</h3>
+      {message.error?<h3 className="error">{message.error}</h3>:message.success?<h3 className="success">{message.success}</h3>:<></>}
       {props.signup?<input type="text" name="name" value={credentials.name} onChange={updateValue} placeholder="Name" required/>:null}
       <input type="email" name="email" value={credentials.email} onChange={updateValue} placeholder="Email" required/>
       <input type="password" name="password" value={credentials.password} onChange={updateValue} placeholder="Password" required/>
